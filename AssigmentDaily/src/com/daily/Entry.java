feb14e573c62577367113565a4165faa7f281463
Gopi2147 <86350291+Gopi2147@users.noreply.github.com>
2021-06-30 23:06:59 +0530

    Add files via upload

---
+++ AssigmentDaily/src/com/daily/Entry.java
@@ -0,0 +1,33 @@
+package com.daily;
+
+import java.util.LinkedHashSet;
+
+public class Entry {
+//	LinkedHashSet<String> lhs= new LinkedHashSet<String>();
+	private String name;	
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;		
+	}
+	public Entry(int id, String name) {
+		super();
+		this.name = name;
+		this.id= id;
+	}
+	@Override
+	public String toString() {
+		return "Entry [name=" + name + ", id=" + id + "]";
+	}
+	public Entry() {
+		super();
+	}	
+}
